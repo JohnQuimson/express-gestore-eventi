9739706a94b3d27852f0341ceeb5d0986b7e211e
JohnQuimson <john.quimson777@gmail.com>
2024-05-31 15:53:39 +0200

    feat: db & getReservation function

--- models/reservation.js
+++ models/reservation.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 
+const filePath = path.join(__dirname, '../db/reservations.json');
+
 class Reservation {
   constructor(id, firstName, lastName, email, eventId) {
     this.id = id;
@@ -8,5 +10,9 @@ class Reservation {
     this.lastName = lastName;
     this.email = email;
     this.eventId = eventId;
+  }
+
+  static getReservations() {
+    return require(filePath);
   }
 }
