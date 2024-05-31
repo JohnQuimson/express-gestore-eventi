475edd7fc27a2668baf8dd0c4cb8299cd14c5731
JohnQuimson <john.quimson777@gmail.com>
2024-05-31 15:38:00 +0200

    feat: scaffold reservation (routes + importations)

---
+++ controllers/reservations.js
@@ -0,0 +1,13 @@
+const Reservation = require('../models/reservation.js');
+
+const index = (req, res, next) => {};
+
+const store = (req, res, next) => {};
+
+const destroy = (req, res, next) => {};
+
+module.exports = {
+  index,
+  store,
+  destroy,
+};
--- routers/events.js
+++ routers/events.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const eventsController = require('../controllers/events.js');
+const reservationsControllers = require('../controllers/reservations.js');
 
 router.get('/', eventsController.index);
 router.get('/:id', eventsController.show);
 router.post('/', eventsController.store);
 router.put('/:event', eventsController.update);
+
+// reservation
+router.get('/:event/reservations', reservationsControllers.index);
+router.post('/:event/reservations', reservationsControllers.store);
+router.delete(
+  '/:event/reservations/:reservation',
+  reservationsControllers.destroy
+);
 
 module.exports = router;
