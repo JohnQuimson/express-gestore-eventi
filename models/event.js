c076f6bd0e24c6002bb256d29460b33a264e0e4e
JohnQuimson <john.quimson777@gmail.com>
2024-05-31 11:44:27 +0200

    feat: milestone 2

--- models/event.js
+++ models/event.js
@@ -1,3 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+const { rejects } = require('assert');
+
+const filePath = path.join(__dirname, '../db/events.js');
+
 class Event {
   constructor(id, title, description, date, maxSeats) {
     this.id = id;
@@ -6,4 +12,36 @@ class Event {
     this.date = date;
     this.maxSeats = maxSeats;
   }
+
+  // read data
+  static getEvent() {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, (error, data) => {
+        if (error) {
+          return reject(error);
+        }
+        const events = JSON.parse(data);
+        resolve(events);
+      });
+    });
+  }
+
+  // save data
+  static saveEvent(event) {
+    return new Promise((resolve, reject) => {
+      Event.getEvent()
+        .then((events) => {
+          events.push(event);
+          fs.writeFile(filePath, JSON.stringify(events), (error) => {
+            if (error) {
+              return reject(error);
+            }
+            resolve();
+          });
+        })
+        .catch((error) => reject(error));
+    });
+  }
 }
+
+module.exports = Event;
